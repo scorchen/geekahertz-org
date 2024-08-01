a11b3c52e537273a7815cf9b9eaa8fa4aed3a9d3
Justin Eyre <scorchen@gmail.com>
2024-08-01 09:13:23 -0600

    Logo

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
                 <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
             <body className="antialiased text-white bg-blue-900">
-                <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
+                <div className="flex flex-col min-h-screen px-6  sm:px-12">
                     <div className="flex flex-col w-full max-w-5xl mx-auto grow">
                         <Header />
                         <div className="grow">{children}</div>
--- components/header.jsx
+++ components/header.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import netlifyLogo from 'public/netlify-logo.svg';
+import netlifyLogo from 'public/hello.geekahertz.jpg';
 import githubLogo from 'public/images/github-mark-white.svg';
 
 const navItems = [
@@ -16,7 +16,7 @@ export function Header() {
     return (
         <nav className="flex flex-wrap items-center gap-4 pt-6 pb-12 sm:pt-12 md:pb-24">
             <Link href="/">
-                <Image src={netlifyLogo} alt="Netlify logo" />
+                <Image src={netlifyLogo} alt="geekahertz" />
             </Link>
             {!!navItems?.length && (
                 <ul className="flex flex-wrap gap-x-4 gap-y-1">
@@ -32,7 +32,7 @@ export function Header() {
                     ))}
                 </ul>
             )}
-            <div className="flex-grow justify-end hidden lg:flex lg:mr-1">
+            {/* <div className="flex-grow justify-end hidden lg:flex lg:mr-1">
                 <Link
                     href="https://github.com/netlify-templates/next-platform-starter"
                     target="_blank"
@@ -40,7 +40,7 @@ export function Header() {
                 >
                     <Image src={githubLogo} alt="GitHub logo" className="w-7" />
                 </Link>
-            </div>
+            </div> */}
         </nav>
     );
 }
